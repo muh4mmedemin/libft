06299150cad900043844f8df0581a3a92c7855f8
dark <dark>
2025-06-11 08:59:08 +0300

    Added ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    int i;
+    char *str;
+
+    i = 0;
+    if (f == NULL)
+        return NULL;
+    str = malloc(ft_strlen((char *)s) + 1);
+    if (str == NULL)
+        return NULL;
+    while (s[i])
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0'; 
+    return str;
+}
