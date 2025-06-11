78f632563eacbf15780d95af0ccb60fe8e2fc0f1
dark <dark@dark.com>
2025-06-11 13:24:34 +0300

    Norm fix

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -13,22 +13,22 @@
 #include <stdlib.h>
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    int i;
-    char *str;
+	int		i;
+	char	*str;
 
-    i = 0;
-    if (f == NULL)
-        return NULL;
-    str = malloc(ft_strlen((char *)s) + 1);
-    if (str == NULL)
-        return NULL;
-    while (s[i])
-    {
-        str[i] = f(i, s[i]);
-        i++;
-    }
-    str[i] = '\0'; 
-    return str;
+	i = 0;
+	if (f == NULL)
+		return (NULL);
+	str = malloc(ft_strlen((char *)s) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
