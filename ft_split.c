16d152c87ce18a36c38c62d0f4ceed2da96f725f
dark <dark@dark.com>
2025-06-11 08:26:52 +0300

    Norm fix

--- ft_split.c
+++ ft_split.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 
 int	countw(char *s, char sep)
