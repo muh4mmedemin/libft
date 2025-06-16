7097b99bfce111238ac2c094a47b445016ce0d82
Muhammed Emin Ayna <muayna@k1m09s01.42istanbul.com.tr>
2025-06-16 14:31:31 +0300

    Bug fix

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,13 @@
 /*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 22:49:54 by muayna            #+#    #+#             */
-/*   Updated: 2025/06/14 11:44:53 by muayna           ###   ########.fr       */
+/*   Updated: 2025/06/16 14:30:45 by muayna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int	countw(char *s, char sep)
 {
@@ -29,50 +31,72 @@ int	countw(char *s, char sep)
 	return (word);
 }
 
-char	*char_malloc(char *s, char c)
+int	chr_size(char *s, char sep, int *e)
 {
-	int		size;
-	int		size_temp;
-	char	*temp;
-
-	size = 0;
-	while (s[size] != c && s[size] != '\0')
-		size++;
-	size_temp = size;
-	temp = malloc(size + 1);
-	temp[size] = '\0';
-	size = 0;
-	while (size < size_temp)
+	int size;
+	while (s[*e])
 	{
-		temp[size] = s[size];
-		size++;
+		while (s[*e] == sep)
+			(*e)++;
+		size = 0;
+		while (s[*e] != sep && s[*e] != '\0')
+		{
+			(*e)++;
+			size++;
+			if(s[*e] == sep)
+				return size;
+		}
 	}
-	return (temp);
+	return size;
 }
 
-char	**ft_split(char const *s, char c)
+void fill (int *e, char *s, char sep, char **str)
 {
-	int		i;
-	int		b;
-	int		word_size;
-	char	**str;
+	int i;
+	int b;
+	i = 0;
+	while (str[i] != NULL)
+	{
+		while(s[*e] == sep)
+			(*e)++;
+		b = 0;
+		while(s[*e] != sep && s[*e] != '\0')
+		{
+			str[i][b] = s[*e];
+			(*e)++;
+			b++;
+			str[i][b] = '\0';
+		}
+		i++;
+	}
+}
+
+char **ft_split(char const *s, char c)
+{
+	int wr_size;
+	char **str;
+	int e;
+	int b;
 
 	b = 0;
-	word_size = countw((char *)s, c);
-	i = 0;
-	str = malloc((word_size + 1) * sizeof(char *));
-	while (i < word_size)
+	e = 0;
+	wr_size = countw((char *)s, c);
+	str = malloc(8 * (wr_size + 1));
+	str[wr_size] = NULL;
+	while (b < wr_size)
 	{
-		if (s[b] != c && s[b] != '\0')
-		{
-			str[i] = char_malloc((char *)&s[b], c);
-			i++;
-		}
-		while (s[b] != c && s[b] != '\0')
-			b++;
-		while (s[b] == c && s[b] != '\0')
-			b++;
+		str[b++] = malloc(chr_size((char *)s, c, &e) + 1);
+		if (str[b - 1] == NULL)
+			while (b != -1)
+			{
+				b--;
+				free(str[b]);	
+			}
+		if (b == -1)
+			return NULL;
 	}
-	str[i] = NULL;
-	return (str);
+	e = 0;
+	fill(&e, (char *)s, c, str);
+	return str;
 }
+
