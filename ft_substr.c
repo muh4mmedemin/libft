a571b4f97420470b2a4f26f35e6c8084d377dae7
Muhammed Emin Ayna <muayna@k1m24s02.42istanbul.com.tr>
2025-06-17 13:07:37 +0300

    Bug fix

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 14:47:34 by muayna            #+#    #+#             */
-/*   Updated: 2025/06/16 19:48:10 by muayna           ###   ########.fr       */
+/*   Updated: 2025/06/17 13:07:24 by muayna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*substr;
 	size_t			i;
-	size_t			b;
-
-	b = ft_strlen((char *)s);
-	if (s == NULL || start >= len || b <= start)
-		return ft_strdup("");
-	b = b - start;
-	if (b > len)
-		substr = malloc(len + 1);
-	if (substr == NULL)
-		return NULL;
-	else
-		substr = malloc(b + 1);
-	if (substr == NULL)
-		return NULL;
+	size_t b = 0;
 	i = 0;
-	while (s[i] && i < len)
-		substr[i++] = s[start++];
+	size_t size = ft_strlen(s);
+	if (len == 0 || s == NULL || start > size)
+		return ft_strdup("");
+	if (len >= size)
+		b = size - start;
+	else
+		b = len;
+	substr = malloc(b + 1);
+	if (substr == NULL)
+		return NULL;
+	while (start < size && i < b)
+	{
+		substr[i] = s[start];
+		start++;
+		i++;
+	}
 	substr[i] = '\0';
-	return (substr);
+	return substr;
 }
